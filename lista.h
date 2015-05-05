a56beb7c62b3d590815eee80701f0ee4ee664b44
Rodrigo Rato <rodrigorato1996@hotmail.com>
2015-05-05 18:37:41 +0100

    Commit lel

---
+++ lista.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "clientes.h"
+
+typedef struct node* link;
+
+struct node{
+	Cli cliente;
+	link next;
+};
+
+typedef struct LISTA{
+
+}lista;
+
+novalista, insereordenado, pesquisa, eliminacliente, limpalista
+
