3c38c49a5a01ae61eea02e901a6e41f814ce5778
Alice <alice@alice-virtual-machine.(none)>
2015-05-06 16:21:54 +0100

    a fila agora tem o nº de cheques

--- fila.h
+++ fila.h
@@ -7,27 +7,28 @@
 #include "cheques.h"
 #include "dados.h"
 
-
-
-typedef struct {
-	link_f head;
-	link_f tail;
-} fila;
-
 typedef struct fila_node* link_f;
 
 struct fila_node{
 	Chq check;
-	link next;
+	link_f next;
 };
 
-link_f new_fila_node(Chq cheque);
+typedef struct {
+	link_f head;
+	link_f tail;
+	int    nche;
+} fila;
+
+
 fila* nova_fila();
 void insertCheck(fila* f,Chq cheque);
 int fila_vazia(fila* f);
 Chq tira_first(fila* f);
-Chq search_and_destroy(fila* f, Ref ref);
-Chq search_fila(fila* f,Ref ref);
+Chq search_and_destroy(fila* f, Ref ref);/*vai ser usado com o comando processaR,
+                                          se nao encontrar devolve cheque invalido*/
+Chq search_fila(fila* f,Ref ref);/*vai ser usado com o comando infocheque*/
+
 
 
 
