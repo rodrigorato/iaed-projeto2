a3bf2422194b77669eea7ceddc657099ce848ec8
Rodrigo Rato <rodrigorato1996@hotmail.com>
2015-05-06 16:29:34 +0100

    whitespace

--- cheques.h
+++ cheques.h
@@ -21,8 +21,6 @@ Ref refeCheque(Chq cheque);
 Ref refbCheque(Chq cheque);
 int chequeValido(Chq cheque); /* 0 se o cheque for invalido, qualquer outro valor em caso contrario */
 void imprimeCheque(Chq cheque);
-
-
 /* void escreveCheque(Chq ch1); - eventualmente useless */
 
 
