80871e7d3cc6940a183aaf146cb6fa71f11a7f62
Nuno Vieira <nuno@Nunz.(none)>
2015-05-06 11:44:19 +0100

    mostly worte whats missing, clientes à partida vao passar a estar numa arvore e nao numa lista como planeado. Faltam tambem algumas funções em relação ao ADT cliente.

--- main.c
+++ main.c
@@ -42,34 +42,46 @@ int main(){
 			case 6: 
 			/*cheque*/
 				scanf("%u %lu %lu %lu",&valor,&refe,&refb,&refc);
-				/*criaCheque vai devolver o apontador do novo cheque para ser posto na fila(tail)*/
-				criaCheque(criaValor(valor),
-						   criaReferencia(refe),
-						   criaReferencia(refb),
-						   criaReferencia(refc));
+				/*criaCheque vai devolver o apontador do novo cheque para ser posto na fila*/
+				/* tenho que criar o node ou nao? */
+				new_fila_node(criaCheque(criaValor(valor),
+							  			 criaReferencia(refe),
+						   	  			 criaReferencia(refb),
+						   	  			 criaReferencia(refc));
+
+				/* saber se cliente emissor e/ou benefeciente existem*/
+				/*caso existam , apenas somar os valores aos respectivos clientes*/
+				/* caso nao exista(m) cria-los*/
 
 				break;
 
 			case 8:
 			/*processa*/
+				tira_first(Queue_chq);
+				/* preciso de ir buscar os valores e ref do cheque devolvido*/
 				
 				break;
 
 			case 9:
 			/*processaR*/
-				scanf("%d",refc);
-
+				scanf("%lu",&refc);
+				search_and_destroy(Queue_chq, criaReferencia(refc));
+				/* preciso de ir buscar o valor e refs do cheque devolvido*/
 				break;
 
 			case 10:
 			/*infocheque*/
-				scanf("%d",refc);
-
+				scanf("%lu",&refc);
+				printf("Cheque-info: ");
+				escreveCheque(search_fila(Queue_chq, criaReferencia(refc)));
+				putchar('\t');
 				break;
 
 			case 11:
 			/*infocliente*/
-				scanf("%d",refc);
+				scanf("%d",&refc); /*refc neste caso stands for referencia cliente*/
+				/*falta funcao de escrita do cliente*/
+				criaReferencia(refc)
 
 				break;
 
