dc9b180d93dacfce7691c8cb90ddeff49753b67b
Rodrigo Rato <rodrigorato1996@hotmail.com>
2015-05-08 15:53:48 +0100

    Comentarios.

--- avl.h
+++ avl.h
@@ -21,37 +21,39 @@ typedef struct {
 	link root;
 } arvore;
 
-/* Cria uma nova arvore vazia e devolve o link inicial */
+/* Cria e devolve uma nova arvore, vazia. */
 arvore* novaArvore(); 
 
-/* Devolve o numero de elementos na arvore apontada por h */
+/* Devolve o numero de elementos na arvore. */
 int numElementos(arvore* avltree); 
 
-/* Devolve um ponteiro para o primeiro cliente que existe
-   na arvore cuja referencia e a dada. Devolve o cliente invalido
-   caso nao exista. */
+/* Procura na arvore um cliente com referencia igual
+   a que recebe. Caso nao exista devolve o cliente
+   invalido (referencia = -1). 
+   E necessario libertar o cliente invalido mais tarde. */
 Cli procuraElemento(arvore* avltree, Ref referencia); 
 
-/* Insere o novo Cliente na arvore.
-    */
+/* Insere um novo cliente na arvore.
+   Apenas faz com que o novo no da arvore
+   fique com o cliente que recebe, nao guarda
+   uma copia do mesmo. */
 void insereElemento(arvore* avltree, Cli cliente);
 
 /* Liberta a memoria associada ao cliente
-   da arvore cuja referencia e a referencia dada- */
+   da arvore cuja referencia e a referencia dada. */
 void apagaElemento(arvore* avltree, Ref referencia);
 
-/* Liberta toda a memoria associada a arvore
-   cuja raiz e apontada por h. 
-   NESTE MOMENTO ESTA A LIBERTAR A ARVORE TODA
-   NUNO? ISTO E PRECISO? */
+/* Liberta toda a memoria associada a arvore. */
 void cutdownArvore(arvore* avltree);
 
-/* Escreve no ecra todos os clientes na arvore 
-   apontada por h no formato pedido. */
+/* Escreve no ecra todos os clientes que existem
+   na arvore de forma ordenada e cada um
+   numa linha diferente. */
 void escreveClientesInorder(arvore* avltree);
 
-/* As seguintes funcoes nao sao necessarias fora deste
-   ficheiro - os comentarios estao no proprio codigo. */
+/* Funcoes usadas na construcao e manutencao
+   da arvore. Nao sera necessario usa-las fora 
+   do ficheiro .c onde esta implementado este tipo. */
 link novoNode(Cli cliente, link l, link r);
 link max(link h);
 link min(link h);
@@ -65,7 +67,7 @@ int balance(link h);
 link AVLbalance(link h);
 link insertR(link h, Cli cliente);
 link deleteR(link h, Ref referencia);
-link procura(link h, Ref referencia); /* Atencao com este gajo! */
+link procura(link h, Ref referencia);
 link freeR(link h);
 void traverseInOrder(link h);
 
