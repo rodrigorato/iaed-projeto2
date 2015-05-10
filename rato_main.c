f12a6474896927aee3f2e23373c14feb759c2215
rodrigorato <rodrigorato1996@hotmail.com>
2015-05-10 15:38:59 +0100

    Building it - vou testar agora...
    Yes...

--- rato_main.c
+++ rato_main.c
@@ -8,6 +8,64 @@
 #include "fila.h"
 #include "avl.h"
 
+
+int comando(char* nome){
+	int i;
+	char *ops[] = ["cheque",		/* id = 0 */
+				   "processa",		/* id = 1 */
+				   "processaR",		/* id = 2 */
+				   "infocheque",	/* id = 3 */
+				   "infocliente",	/* id = 4 */
+				   "info",			/* id = 5 */
+				   "sair"];			/* id = 6 */
+				   
+	for(i=0; i <= 6; i++)
+		if(!strcmp(nome, ops[i]))
+			return i
+			
+	return -1;	   					/* comando nao existe */
+}
+
 int main(){
-	// are you ready to rumbleeeee
+	int cod;
+	char op[12];
+	
+	scanf("%s", op);
+	while((cod = comando(op)) != 6){
+		switch(cod){
+			case 0:
+				/* A realizar: cheque */
+				printf("cheque\n");
+				break;
+			case 1:
+				/* A realizar: processa */
+				printf("processa\n");
+				break;
+			case 2:
+				/* A realizar: processaR */
+				printf("processaR\n");
+				break;
+			case 3:
+				/* A realizar: infocheque */
+				printf("infocheque\n");
+				break;
+			case 4:
+				/* A realizar: infocliente */
+				printf("infocliente\n");
+				break;
+			case 5:
+				/* A realizar: info */
+				printf("info\n");
+				break;
+			case -1:
+				/* A realizar: COMANDO INVALIDO */
+				printf("INVALIDO\n");
+				break;
+		}
+	}
+	
+	/* A realizar: sair */
+	// codigo para sair aqui.
+	
+	return 0;
 }
