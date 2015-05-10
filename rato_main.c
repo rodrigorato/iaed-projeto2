06baea9e5257dbb32f5810a763ea6448c33f5495
rodrigorato <rodrigorato1996@hotmail.com>
2015-05-10 15:23:03 +0100

    vou mase re-escrever isto

---
+++ rato_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dados.h"
+#include "cheques.h"
+#include "clientes.h"
+#include "fila.h"
+#include "avl.h"
+
+int main(){
+	// are you ready to rumbleeeee
+}
